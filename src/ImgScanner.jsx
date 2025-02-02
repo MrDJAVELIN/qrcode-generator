d943d4d3fda68769ea8041ec5aa32f13797b8d25
mrdjavelin <mrdjavelin@gmail.com>
2025-02-02 19:34:30 +0200

    add scanner

---
+++ src/ImgScanner.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import jsQR from "jsqr";
+
+const QRImageScanner = () => {
+    const [qrResult, setQrResult] = useState("");
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        if (!file) return;
+
+        const previewUrl = URL.createObjectURL(file);
+        setImagePreview(previewUrl);
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const img = new Image();
+            img.src = e.target.result;
+            img.onload = () => {
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+
+                const imageData = ctx.getImageData(
+                    0,
+                    0,
+                    canvas.width,
+                    canvas.height
+                );
+                const qrCode = jsQR(
+                    imageData.data,
+                    canvas.width,
+                    canvas.height
+                );
+
+                if (qrCode) {
+                    setQrResult(qrCode.data);
+                } else {
+                    setQrResult("QR-код не найден");
+                }
+
+                URL.revokeObjectURL(previewUrl);
+            };
+        };
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <>
+            <div className="container">
+                <h1>Upload QR-Code</h1>
+
+                <div className="upload">
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageUpload}
+                    />
+                    <img src={imagePreview} alt="" />
+                    <h1>
+                        Result:<br></br>
+                        {qrResult || "Upload..."}
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default QRImageScanner;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import { Routes, Router, Route, Link } from "react-router-dom";
 import QRScanner from "./Scanner";
 import App from "./App";
+import QRImageScanner from "./ImgScanner";
 
 const Root = () => {
     return (
@@ -8,6 +9,7 @@ const Root = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/scanner" element={<QRScanner />} />
+                <Route path="/upload" element={<QRImageScanner />} />
             </Routes>
         </>
     );
